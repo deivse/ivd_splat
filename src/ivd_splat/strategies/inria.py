d6f4614be1292ed622292c5b11f1e21f5e6ee719
Ivan Desiatov <desiaiva@cvut.cz>
2026-05-27 14:43:05 +0200

    Fix INRIAStrategy overrides.

--- src/ivd_splat/strategies/inria.py
+++ src/ivd_splat/strategies/inria.py
@@ -1,10 +1,13 @@
+from dataclasses import dataclass
+
 from ivd_splat.strategies.default_with_gaussian_cap import (
     DefaultWithGaussianCapStrategy,
 )
 
 
+@dataclass
 class INRIAStrategy(DefaultWithGaussianCapStrategy):
     """Just a thin wrapper that sets absgrad to False by default, for convenience."""
 
-    absgrad = False
-    grow_grad2d = 0.0002
+    absgrad: bool = False
+    grow_grad2d: float = 0.0002
